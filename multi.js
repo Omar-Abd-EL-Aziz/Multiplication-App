1d69f6da7a8f162e02a5d570671324563b471d54
omar-abd-el-aziz <shiko55510@gmail.com>
2022-12-06 14:39:53 +0200

    score bug fixed

--- multi.js
+++ multi.js
@@ -1,7 +1,6 @@
 const num1 = Math.ceil(Math.random() * 10);
 const num2 = Math.ceil(Math.random() * 10);
 
-console.log(num1, num2);
 /////////////////////////////////////////////////////////////////////
 let ask = `ما هو حاصل ضرب ${num1} <span>&#215;</span> ${num2} ؟`;
 const question = document.querySelector("h1");
@@ -18,8 +17,9 @@ const scoreEl = document.querySelector("h4");
 scoreEl.innerText = `نتيجتك : ${score}`;
 const total = num1 * num2;
 ///////////////////////////////////////////////////////////////////////
+const btn = document.querySelector(".btn");
 const form = document.querySelector("form");
-form.addEventListener("submit", (event) => {
+btn.addEventListener("click", (event) => {
   event.preventDefault();
   let userValue = +input.value;
   console.log(userValue);
@@ -30,7 +30,6 @@ form.addEventListener("submit", (event) => {
     score--;
     saveScore();
   }
-  console.log(score);
 });
 //////////////////////////////////////////////////////////////////////////
 const saveScore = () => {
@@ -40,18 +39,21 @@ const saveScore = () => {
 /////////////////////////////////////////////////////////////////////////
 const succes = document.querySelector(".succes");
 const wrong = document.querySelector(".wrong");
-console.log(succes, wrong);
-const btn = document.querySelector(".btn");
+
 const right = new Audio("audiomass-output.mp3");
 const faild = new Audio("faild.mp3");
 btn.addEventListener("click", () => {
   if (total === +input.value) {
     succes.style.display = "block";
-    console.log(`right`);
+    setTimeout(() => {
+      succes.style.display = "none";
+    }, 4000);
     right.play();
   } else {
     wrong.style.display = "block";
-    console.log(`Wrong`);
+    setTimeout(() => {
+      wrong.style.display = "none";
+    }, 4000);
     faild.play();
   }
 });
